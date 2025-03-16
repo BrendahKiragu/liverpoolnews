ce129e1a091b2a054d8c72471e2e9783594e499b
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2025-03-16 17:39:05 +0300

    added route for junior players

--- src/app/api/stats/route.js
+++ src/app/api/stats/route.js
@@ -4,22 +4,29 @@ export async function GET(request) {
   try {
     // Fetch players' data from the database
     const players = await prisma.players.findMany();
+    const legends = await prisma.legends.findMany(); // Fetch legends data
 
-    // Return a 404 if no players are found
-    if (!players || players.length === 0) {
-      return new Response(JSON.stringify({ error: "No players found" }), {
-        status: 404,
-        headers: { "Content-Type": "application/json" },
-      });
+    // Check if data exists
+    if (
+      (!players || players.length === 0) &&
+      (!legends || legends.length === 0)
+    ) {
+      return new Response(
+        JSON.stringify({ error: "No players or legends found" }),
+        {
+          status: 404,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
     }
 
-    // Return the players' data as JSON
-    return new Response(JSON.stringify(players), {
+    // Return the players and legends data as JSON
+    return new Response(JSON.stringify({ players, legends }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
-    console.error("Error fetching players:", error);
+    console.error("Error fetching data:", error);
     return new Response(JSON.stringify({ error: "Internal Server Error" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
